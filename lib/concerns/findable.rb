546026a22dff2dba20e1a89dd11162f7e44b01c1
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-10 20:30:27 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,12 @@
 module Findable
   
   def find_by_name
-     def self.find_by_name(name)
+    def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
-  end
+      end
+    def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+      end
   end
 
 end
