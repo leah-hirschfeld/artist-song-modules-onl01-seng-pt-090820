6b3fb1070a5c162dc76073914305011b60fac30d
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-10 20:46:36 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,7 @@
 module Findable
-  attr_accessor :name
 
   def find_by_name(name)
     def find_by_name(name)
-      #ObjectSpace.each_object(name).detect{|a| a.name == name}
       self.all.detect{|a| a.name == name}
     end
   end
