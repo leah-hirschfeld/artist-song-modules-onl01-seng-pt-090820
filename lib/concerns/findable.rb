656b6575fc60e5946a1efcd2e4fe1a7a412e26b5
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-10 20:44:22 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,7 +2,8 @@ module Findable
 
   def find_by_name(name)
     def find_by_name(name)
-      ObjectSpace.each_object(name).detect{|a| a.name == name}
+      #ObjectSpace.each_object(name).detect{|a| a.name == name}
+      Memorable.detect{|a| a.name == name}
     end
   end
 
