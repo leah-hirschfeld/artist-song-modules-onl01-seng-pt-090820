5dd4ad19b912853b3ce91d5c17ebac88a41764c8
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-10 20:19:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,17 +14,21 @@ class Song
     @@songs.detect{|a| a.name == name}
   end
 
+
   def self.all
     @@songs
   end
-
+  
+=begin
   def self.reset_all
     self.all.clear
   end
 
+
   def self.count
     self.all.count
   end
+=end
 
   def artist=(artist)
     @artist = artist
