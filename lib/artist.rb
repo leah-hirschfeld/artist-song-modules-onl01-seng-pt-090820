64fd0faf880e5f623a0f5e643d8b68f2a62e449d
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-10-10 20:58:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,8 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   
+  extend Memorable::ClassMethods
+include Memorable::InstanceMethods
   extend Memorable
   extend Findable
   include Paramable
